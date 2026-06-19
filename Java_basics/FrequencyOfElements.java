bfe3f545aff87dac3ec26b9a3f6e8b1ee97c1f60
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-06-19 08:01:40 +0530

    [Nagayogeswari:Committed Java Basics Problem]

---
+++ Java_basics/FrequencyOfElements.java
@@ -0,0 +1,24 @@
+public class FrequencyOfElements {
+    public static void main(String[] args) {
+
+        int[] arr = {10,20,10,30,20,10};
+
+        for(int i = 0; i < arr.length; i++) {
+
+            int count = 1;
+
+            if(arr[i] == -1)
+                continue;
+
+            for(int j = i+1; j < arr.length; j++) {
+
+                if(arr[i] == arr[j]) {
+                    count++;
+                    arr[j] = -1;
+                }
+            }
+
+            System.out.println(arr[i] + " = " + count);
+        }
+    }
+}
