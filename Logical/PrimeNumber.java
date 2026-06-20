4b9278a79727cbdb25e5ee3fa5680571c995a43d
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-06-20 23:23:49 +0530

    [Nagayogeswari]:Added Prime Number

---
+++ Logical/PrimeNumber.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+
+
+        if(num <= 1) {
+
+            isPrime = false;
+
+        }
+        else {
+
+            for(int i = 2; i < num; i++) {
+
+                if(num % i == 0) {
+
+                    isPrime = false;
+                    break;
+
+                }
+            }
+        }
+
+
+        if(isPrime) {
+
+            System.out.println(num + " is Prime Number");
+
+        }
+        else {
+
+            System.out.println(num + " is not Prime Number");
+
+        }
+
+
+        sc.close();
+    }
+}
